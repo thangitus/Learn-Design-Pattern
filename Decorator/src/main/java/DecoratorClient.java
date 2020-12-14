2e84b71953b32944650bd3b48ef1fd3876334d99
Thang Nguyen <thang.itus@gmail.com>
2020-12-14 23:52:44 +0700

    Add Decorator pattern

---
+++ Decorator/src/main/java/DecoratorClient.java
@@ -0,0 +1,29 @@
+public class DecoratorClient {
+
+   public static void main(String[] args) {
+      System.out.println("NORMAL EMPLOYEE: ");
+      EmployeeComponent employee = new EmployeeConcreteComponent("GPCoder");
+      employee.showBasicInformation();
+      employee.doTask();
+
+      System.out.println("\nTEAM LEADER: ");
+      EmployeeComponent teamLeader = new TeamLeader(employee);
+      teamLeader.showBasicInformation();
+      teamLeader.doTask();
+
+      System.out.println("\nMANAGER: ");
+      EmployeeComponent manager = new Manager(employee);
+      manager.showBasicInformation();
+      manager.doTask();
+
+      System.out.println("\nTEAM LEADER AND MANAGER: ");
+      EmployeeComponent teamLeaderAndManager = new Manager(teamLeader);
+      teamLeaderAndManager.showBasicInformation();
+      teamLeaderAndManager.doTask();
+
+      System.out.println("\nTEAM MEMBER: ");
+      TeamMember teamMember = new TeamMember(teamLeaderAndManager);
+      teamMember.showBasicInformation();
+      teamMember.doTask();
+   }
+}
